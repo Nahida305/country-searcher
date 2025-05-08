7c8560863cf47fe41accb957e0174f48fd29d12b
Nahida305 <nahidaafrinmasuma@gmail.com>
2025-05-09 01:49:05 +0600

    country data

--- index.js
+++ index.js
@@ -10,4 +10,17 @@ function displayCountry(data){
 
     for(var i = 0; i< data.length;i++){
         var newDiv = document.createElement("div");
-        
+        newDiv.innerHTML = 
+        `<div class="country">
+            <img src="${data[i].flags.png}" alt="flag">
+            <h2>${data[i].name.common}</h2>
+            <p>Capital: ${data[i].capital}</p>
+            <p>Population: ${data[i].population}</p>
+            <p>Region: ${data[i].region}</p>
+            <p>Area: ${data[i].area}</p>
+            <p>Currency: ${Object.values(data[i].currencies)[0].name}</p>
+        </div>`;
+        oldContent.appendChild(newDiv);
+    }
+
+}
